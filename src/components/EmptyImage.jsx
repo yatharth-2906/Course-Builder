692dca4f771daf9b6aced727fdc72f7ec3213857
Yatharth <97800277+yatharth-2906@users.noreply.github.com>
2024-06-23 21:01:31 +0530

    Update EmptyImage.jsx

--- src/components/EmptyImage.jsx
+++ src/components/EmptyImage.jsx
@@ -3,7 +3,7 @@ import styles from "./EmptyImage.module.css";
 function EmptyImage() {
   return (
     <div className={styles.empty_image_container}>
-      <img className={styles.empty_image} src="src/assets/empty_image.png" />
+      <img className={styles.empty_image} src="../src/assets/empty_image.png" />
     </div>
   );
 }
